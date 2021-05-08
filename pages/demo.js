cfa8228f70eb8e7be6334a1e0919f1f2a18ecf47
Saber Soleymani <postorder@gmail.com>
2021-05-08 12:17:55 -0400

    test page added

--- pages/demo.js
+++ pages/demo.js
@@ -2,7 +2,10 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+
 export default function Demo() {
+
+  
   return (
     <div className={styles.container}>
       <Head>
